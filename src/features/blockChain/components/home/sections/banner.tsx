28dae5fb74d8559e035c06a2c3c06441b34c3917
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2020-12-15 03:27:35 +0700

    fix reponsive banner

--- src/features/blockChain/components/home/sections/banner.tsx
+++ src/features/blockChain/components/home/sections/banner.tsx
@@ -20,19 +20,23 @@ const useStyles = (props: any) => (makeStyles((theme: Theme) =>
       backgroundSize: 'cover',
     },
     rightElement: {
+      position: 'relative',
       float: 'right',
       marginRight: theme.spacing(2),
       height: '100%',
-      paddingLeft: props.minWidth1400 ? '50px' : theme.spacing(1),
+      minHeight: '460px',
+      width: '60%',
+      paddingLeft: props.minWidth1280 ? '50px' : theme.spacing(1),
     },
     leftElement: {
       float: 'left',
-      maxWidth: '410px',
-      width: '40%',
+      width: props.minWidth1280 ? '40%' : '90%',
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'center',
-      alignItems: 'flex-start'
+      alignItems: 'flex-start',
+      flexBasis: props.minWidth1280 ? 'inherit' : 'auto',
+      maxWidth: props.minWidth1280 ? '410px' : '100%',
     },
     playNowBtn: {
       marginTop: theme.spacing(2),
@@ -49,25 +53,30 @@ const useStyles = (props: any) => (makeStyles((theme: Theme) =>
       fontSize: '16px'
     },
     bannerContent: {
-      display: 'flex'
+      display: 'flex',
+      height: '100%',
+      minHeight: '460px'
     },
     rightElementImage: {
-      marginTop: '68px',
-      height: props.minWidth1400 ? '100%' : 'auto',
-      minHeight: props.minWidth1400 ? '410px' : 'auto',
-      width: props.minWidth1400 ? 'auto' : '100%',
-      bottom: 0
+      height: props.minWidth1280 ? '100%' : 'auto',
+      minHeight: props.minWidth1280 ? '410px' : 'auto',
+      width: props.minWidth1280 ? 'auto' : '100%',
+      bottom: 0,
+      position: 'absolute',
+      left: 0,
+      minWidth: props.minWidth450 ? '410px' : 'auto',
     },
     bannerContentGrid: {
-      height: '100%'
+      height: '100%',
+      minHeight: '460px',
     }
   }),
 ))()
 
 const Banner = (props: BannerProps) => {
-  const minWidth1400 = useMediaQuery('(min-width:1230px)')
-  const minWidth800 = useMediaQuery('(min-width:800px)')
-  const classes = useStyles({ minWidth1400 })
+  const minWidth1280 = useMediaQuery('(min-width:1280px)')
+  const minWidth450 = useMediaQuery('(min-width:450px)')
+  const classes = useStyles({ minWidth1280, minWidth450 })
 
   const leftElement = <>
     <Typography className={classes.title} variant={'h4'}>Fun Blockchain Games that Earn.</Typography>
@@ -80,12 +89,12 @@ const Banner = (props: BannerProps) => {
   return (
     <div className={classes.banner}>
       <Container maxWidth='lg' className={classes.bannerContent}>
-        <Grid item container xs={12} className={classes.bannerContentGrid}>
-          <Grid item xs={6} className={classes.leftElement}>
+        <Grid item container className={classes.bannerContentGrid}>
+          <Grid item xs={5} className={classes.leftElement}>
             {leftElement}
             <Button type={buttonType.Default} className={classes.playNowBtn}>PLAY NOW</Button>
           </Grid>
-          <Grid item xs={6} className={classes.rightElement}>
+          <Grid item xs={7} className={classes.rightElement}>
             {rightElement}
           </Grid>
         </Grid>
