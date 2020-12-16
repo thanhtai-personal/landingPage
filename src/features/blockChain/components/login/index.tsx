3eba759c6bf2e2b8cc1066c518b6978652f99082
Tai Tran <tatran@deloitte.ca>
2020-12-16 15:08:20 +0700

    login chnage

--- src/features/blockChain/components/login/index.tsx
+++ src/features/blockChain/components/login/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Container, Paper } from '@material-ui/core'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import backgroundImage from 'root/asserts/images/banner.jpg'
 
@@ -7,7 +6,7 @@ const useStyles = (props: any) => (makeStyles((theme: Theme) =>
   createStyles({
     root: {
       width: '100vw',
-      height: 'calc(100vh - 70px)',
+      height: 'calc(100vh)',
       backgroundImage: `url(${backgroundImage})`,
       backgroundSize: 'cover'
     }
